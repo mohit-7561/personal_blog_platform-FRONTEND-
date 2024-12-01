6a15c4a455eae5432a221cab7d1dda70120e2bcd
mohit-7561 <mohitsinghania666r@gmail.com>
2024-12-01 15:20:42 +0530

    added API

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5050/api";
+const API_URL = "https://server-side-vkfa.onrender.com/api";
 
 export const registerUser = (userData) => {
   return axios.post(`${API_URL}/auth/register`, userData);
